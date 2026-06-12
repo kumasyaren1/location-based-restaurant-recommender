16bc208e690e56771283f3ed118c8e6e4968d01b
kumasyaren1 <yaren5433@gmail.com>
2026-06-12 20:46:08 +0300

    Add user geolocation support

--- Components/MapView.tsx
+++ Components/MapView.tsx
@@ -1,23 +1,73 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-export default function MapView() {
-  return (
-    <MapContainer
-      center={[39.7767, 30.5206]}
-      zoom={13}
-      style={{ height: "500px", width: "100%" }}
-    >
-      <TileLayer
-        attribution='&copy; OpenStreetMap contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+type Location = {
+  lat: number;
+  lng: number;
+};
 
-      <Marker position={[39.7767, 30.5206]}>
-        <Popup>Eskişehir Merkez</Popup>
-      </Marker>
-    </MapContainer>
+const defaultLocation: Location = {
+  lat: 39.7767,
+  lng: 30.5206,
+};
+
+function ChangeMapView({ location }: { location: Location }) {
+  const map = useMap();
+
+  useEffect(() => {
+    map.setView([location.lat, location.lng], 14);
+  }, [location, map]);
+
+  return null;
+}
+
+export default function MapView() {
+  const [userLocation, setUserLocation] = useState<Location>(defaultLocation);
+  const [locationStatus, setLocationStatus] = useState("Konum alınıyor...");
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setLocationStatus("Tarayıcınız konum özelliğini desteklemiyor.");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setUserLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+        setLocationStatus("Konum başarıyla alındı.");
+      },
+      () => {
+        setLocationStatus("Konum izni verilmedi. Varsayılan konum gösteriliyor.");
+      }
+    );
+  }, []);
+
+  return (
+    <section>
+      <p>{locationStatus}</p>
+
+      <MapContainer
+        center={[userLocation.lat, userLocation.lng]}
+        zoom={14}
+        style={{ height: "500px", width: "100%", borderRadius: "12px" }}
+      >
+        <ChangeMapView location={userLocation} />
+
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={[userLocation.lat, userLocation.lng]}>
+          <Popup>Şu anki konumunuz</Popup>
+        </Marker>
+      </MapContainer>
+    </section>
   );
 }
